ee386f2062dc8982d9726df87380f86534512fb2
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-11-07 17:01:34 +0000

    change error message into ts (#4)

---
+++ src/features/error-message/components/ErrorMessage.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+
+type ErrorMessageProps = {
+  message: string;
+};
+export const ErrorMessage: FC<ErrorMessageProps> = ({ message }) => {
+  return <>{message}</>;
+};
