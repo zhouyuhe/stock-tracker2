51859d07fe537e93b782775728576e3825f574af
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-11-08 12:09:49 +0000

    change action and reducer

---
+++ src/features/latest-news/redux/actions.ts
@@ -0,0 +1,20 @@
+import { UPDATE_LATEST_NEWS } from "./constants";
+import { ActionWithPayload } from "../../../store/actions";
+
+export type LatestNewsData = {
+  headline: string;
+  source: string;
+  date: string;
+  url: string;
+};
+
+export type UpdateLatestNewsAction = ActionWithPayload<
+  typeof UPDATE_LATEST_NEWS,
+  LatestNewsData
+>;
+export const updateLatestNewsAction = (
+  latestNews: LatestNewsData
+): UpdateLatestNewsAction => ({
+  type: UPDATE_LATEST_NEWS,
+  payload: latestNews
+});
