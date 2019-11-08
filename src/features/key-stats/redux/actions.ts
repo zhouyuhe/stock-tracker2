903c5d2787f57bd4e1c0391e0c2426884c52988c
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-11-08 13:50:32 +0000

    Feature/ts key stats (#6)
    * change keystats into ts
    
    * change test into typescript
    
    * add specific name to data
    
    * delete the global string object

---
+++ src/features/key-stats/redux/actions.ts
@@ -0,0 +1,33 @@
+import { UPDATE_KEY_STATS } from "./constants";
+import { ActionWithPayload } from "../../../store/actions";
+
+export type KeyStatsData = {
+  companyName: string;
+  symbol: string;
+  currency: string;
+  primaryExchange: string;
+  open: number;
+  high: number;
+  low: number;
+  previousClose: number;
+  previousVolume: number;
+  avgTotalVolume: number;
+  marketCap: number;
+  peRatio: number;
+  week52High: number;
+  week52Low: number;
+  ytdChange: number;
+  isUSMarketOpen: string;
+  eps: number;
+};
+export type UpdateKeyStatsAction = ActionWithPayload<
+  typeof UPDATE_KEY_STATS,
+  KeyStatsData
+>;
+
+export const updateKeyStatsAction = (
+  keyStats: KeyStatsData
+): UpdateKeyStatsAction => ({
+  type: UPDATE_KEY_STATS,
+  payload: keyStats
+});
---
+++ src/features/key-stats/redux/middleware.ts
@@ -0,0 +1,25 @@
+import { BOOTSTRAP } from "../../../store/constants";
+import { updateKeyStatsAction, KeyStatsData } from "./actions";
+import { SocketService } from "../../../services/socketService";
+import { Middleware } from "redux";
+import { AppState } from "../../../store";
+
+export type Dependencies = {
+  socketService: SocketService;
+};
+
+export type KeyStatsMiddleware = (
+  dependencies: Dependencies
+) => Middleware<{}, AppState>;
+
+export const keyStatsMiddleware: KeyStatsMiddleware = ({
+  socketService
+}) => store => next => action => {
+  if (action.type === BOOTSTRAP) {
+    const socket = socketService.get();
+    socket.on("keyStats", (payload: KeyStatsData) => {
+      store.dispatch(updateKeyStatsAction(payload));
+    });
+  }
+  return next(action);
+};
