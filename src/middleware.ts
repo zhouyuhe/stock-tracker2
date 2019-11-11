28f1fabfb719d9ecd135f8812c9df733eb4a3065
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-11-11 15:34:37 +0000

    change app to TS

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { UPDATE_SELECTED_STOCK } from "./store/constants";
+import { resetAction } from "./store/actions";
+import { SocketService } from "./services/socketService";
+import { Middleware } from "redux";
+import { AppState } from "./store";
+
+export type Dependencies = {
+  socketService: SocketService;
+};
+export type StockMiddleware = (
+  dependencies: Dependencies
+) => Middleware<{}, AppState>;
+export const stockMiddleware: StockMiddleware = ({
+  socketService
+}) => store => next => action => {
+  if (action.type === UPDATE_SELECTED_STOCK) {
+    store.dispatch(resetAction());
+    socketService
+      .get()
+      .emit(
+        "stockName",
+        action.payload.symbol,
+        store.getState().chartData.selectedChartRange
+      );
+  }
+  return next(action);
+};
