f1a3c119806b751c5a0198b06696ef978337970a
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-11-11 14:24:12 +0000

    change stock Ticker into TS

--- src/features/market-status/components/MarketStatus.tsx
+++ src/features/market-status/components/MarketStatus.tsx
@@ -2,20 +2,23 @@ import React, { FC } from "react";
 import moment from "moment";
 import "./MarketStatus.css";
 import { KeyStatsData } from "../../key-stats/redux/actions";
+import { StockTickerData } from "../../stock-ticker/redux/actions";
 
 type MarketStatusProps = {
-  stock: any;
+  stock: StockTickerData | undefined;
   keyStats: KeyStatsData | null;
 };
 const marketSign = (value: number) =>
   value === null ? "market---moon" : "market---sun";
 const formatDate = (date: Date) => new Date(date);
 export const MarketStatus: FC<MarketStatusProps> = ({ stock, keyStats }) => {
-  const UKTime = formatDate(stock.latestUpdate);
-  const USTime = formatDate(UKTime).toLocaleString("en-US", {
-    timeZone: "America/New_York"
-  });
-  const correctFormat = moment(new Date(USTime)).format("lll");
+  const UKTime = stock && formatDate(stock.latestUpdate);
+  const USTime =
+    UKTime &&
+    formatDate(UKTime).toLocaleString("en-US", {
+      timeZone: "America/New_York"
+    });
+  const correctFormat = USTime && moment(new Date(USTime)).format("lll");
   const marketStatus =
     keyStats && keyStats.open === null ? "Market Closed" : "Market Open";
 
---
+++ src/features/stock-ticker/redux/reducer.ts
@@ -0,0 +1,26 @@
+import { RESET } from "../../../store/constants";
+import { UPDATE_STOCK_TICKER } from "./constants";
+import { Reducer } from "redux";
+import { UpdateStockTickerAction, StockTickerData } from "./actions";
+import { ResetAction } from "../../../store/actions";
+
+export type StockTickerState = {
+  selectedStockTicker: StockTickerData | undefined;
+};
+const initialState: StockTickerState = { selectedStockTicker: undefined };
+export const stockTickerReducer: Reducer<
+  StockTickerState,
+  UpdateStockTickerAction | ResetAction
+> = (state = initialState, action) => {
+  switch (action.type) {
+    case UPDATE_STOCK_TICKER:
+      return {
+        ...state,
+        selectedStockTicker: action.payload
+      };
+    case RESET:
+      return { ...initialState };
+    default:
+      return state;
+  }
+};
