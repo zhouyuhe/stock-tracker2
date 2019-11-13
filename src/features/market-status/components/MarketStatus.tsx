b2d238144300a34a9e13a7fe79a1fa672c872060
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-11-13 13:44:40 +0000

    pass less data in

--- src/features/market-status/components/MarketStatus.tsx
+++ src/features/market-status/components/MarketStatus.tsx
@@ -5,29 +5,27 @@ import { KeyStatsData } from "../../key-stats/redux/actions";
 import { StockTickerData } from "../../stock-ticker/redux/actions";
 
 type MarketStatusProps = {
-  stock: StockTickerData | undefined;
-  keyStats: KeyStatsData | null;
+  stock: StockTickerData["latestUpdate"];
+  isMarketOpen: KeyStatsData["isUSMarketOpen"];
 };
 const marketSign = (value: boolean) =>
   value === false ? "market---moon" : "market---sun";
 
-export const MarketStatus: FC<MarketStatusProps> = ({ stock, keyStats }) => {
-  const correctFormat = stock && moment(stock.latestUpdate).format("lll");
-  const marketStatus =
-    keyStats && keyStats.isUSMarketOpen === false
-      ? "Market Closed"
-      : "Market Open";
+export const MarketStatus: FC<MarketStatusProps> = ({
+  stock,
+  isMarketOpen
+}) => {
+  const correctFormat = stock && moment(stock).format("lll");
+  const marketStatus = !isMarketOpen ? "Market Closed" : "Market Open";
 
   return (
     <div className="market">
       <p>
         <span className="market__text">
           Real-Time Price as of {correctFormat} EST
-        </span>{" "}
-        {keyStats && (
-          <span className={marketSign(keyStats.isUSMarketOpen)}>
-            {marketStatus}
-          </span>
+        </span>
+        {isMarketOpen && (
+          <span className={marketSign(isMarketOpen)}>{marketStatus}</span>
         )}
       </p>
     </div>
