d28d27ef8a94cc2ddf2d8c3d10d342f0675e640a
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-11-20 16:33:58 +0000

    Test/chart middleware (#31)
    * set up before each
    
    * on and emit tested
    
    * chart middleware now test pass
    
    * add more descriptions
    
    * code consistency

---
+++ src/features/chart/redux/__test__/middleware.test.ts
@@ -0,0 +1,59 @@
+import { ChartData } from "../actions";
+import { BOOTSTRAP } from "store";
+import { chartMiddleware } from "../middleware";
+import { UPDATE_CHART_RANGE } from "../constants";
+
+describe("Testing Chart Middleware", () => {
+  let mockSocket: any;
+  let socketService: any;
+  let store: any;
+  let next: jest.Mock;
+
+  beforeEach(() => {
+    const payload = [{ date: "123", close: 23 }];
+    mockSocket = {
+      on: jest.fn((name: string, callback: (payload: ChartData[]) => void) => {
+        callback(payload);
+      }),
+      emit: jest.fn()
+    };
+    socketService = {
+      get: () => mockSocket
+    };
+
+    const stockState = {
+      stockData: { selectedStock: { name: "APPLE", symbol: "AAPL" } }
+    };
+
+    store = {
+      getState: jest.fn(() => stockState),
+      dispatch: jest.fn()
+    };
+    next = jest.fn();
+  });
+
+  test("socket.on been called with a function", () => {
+    const action = { type: BOOTSTRAP };
+    const mockChartMiddleware = chartMiddleware({ socketService });
+    mockChartMiddleware(store)(next)(action);
+    expect(mockSocket.on).toBeCalledWith("chartData", expect.anything());
+  });
+
+  test("dispatch has been called with updateAction", () => {
+    const action = { type: BOOTSTRAP };
+    const mockChartMiddleware = chartMiddleware({ socketService });
+    const updateAction = {
+      payload: [{ date: "123", close: 23 }],
+      type: "UPDATE_CHART_DATA"
+    };
+    mockChartMiddleware(store)(next)(action);
+    expect(store.dispatch).toBeCalledWith(updateAction);
+  });
+
+  test("socket.emit been called with timeRange,AAPL, 1Y", () => {
+    const action = { type: UPDATE_CHART_RANGE, payload: "1Y" };
+    const mockChartMiddleware = chartMiddleware({ socketService });
+    mockChartMiddleware(store)(next)(action);
+    expect(mockSocket.emit).toBeCalledWith("timeRange", "AAPL", "1Y");
+  });
+});
