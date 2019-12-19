394fb3ee4a3e7a9ea53317ab941b4b55c2f74fdb
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-12-19 14:42:39 +0000

    another way but not good enough

--- src/App.js
+++ src/App.js
@@ -1,52 +1,41 @@
-import React, { useState, useEffect, useRef } from "react";
-import Feed from "./feed";
-
+import React, { useState, useEffect, useRef, useReducer } from "react";
 import "App1.css";
+import feed from "./feed";
 
-const useStocks = () => {
+const App = () => {
   const [search, setSearch] = useState("");
   const [stocks, setStocks] = useState({});
-  const ref = useRef(new Set());
 
-  useEffect(() => {
-    Feed.onChange(data => {
-      setStocks(prevStocks => {
-        console.log(data);
-        if (!ref.current.has(data.symbol)) return;
-
-        const change = prevStocks[data.symbol]
-          ? prevStocks[data.symbol].last - data.last
-          : 0;
-        const newStocks = {
-          ...prevStocks,
-          [data.symbol]: { ...data, change: change.toFixed(2) }
-        };
-        return newStocks;
-      });
-    });
-  }, []);
-
-  const addToStocks = () => {
-    Feed.subscribe(search);
-    ref.current.add(search.toUpperCase());
+  const onSubscribe = () => {
+    feed.subscribe(search);
     setSearch("");
   };
 
-  console.log(ref.current);
-  //   console.log(stocks)
-
   const unsubscribe = symbol => {
-    Feed.unsubscribe(symbol);
+    feed.unsubscribe(symbol);
     const latestStocks = { ...stocks };
-    delete latestStocks[symbol];
+    latestStocks[symbol] = { symbol, last: null, high: null };
     setStocks(latestStocks);
-    ref.current.delete(symbol);
   };
-  return { stocks, search, setSearch, addToStocks, unsubscribe };
-};
 
-const App = () => {
-  const { stocks, search, setSearch, addToStocks, unsubscribe } = useStocks();
+  console.log(stocks);
+  useEffect(() => {
+    feed.onChange(data => {
+      setStocks(prevStocks => {
+        if (prevStocks[data.symbol] && prevStocks[data.symbol].high === null) {
+          delete prevStocks[data.symbol];
+          return prevStocks;
+        } else {
+          const latestStocks = { ...prevStocks };
+          const change = prevStocks[data.symbol]
+            ? data.last - prevStocks[data.symbol].last
+            : 0;
+          latestStocks[data.symbol] = { ...data, change: change.toFixed(2) };
+          return latestStocks;
+        }
+      });
+    });
+  }, []);
 
   return (
     <>
@@ -56,7 +45,7 @@ const App = () => {
           onChange={e => setSearch(e.target.value)}
           placeholder="Please Enter Stock Here..."
         />
-        <button onClick={addToStocks}>Submit</button>
+        <button onClick={onSubscribe}>Submit</button>
         <table>
           <thead>
             <tr>
@@ -69,22 +58,23 @@ const App = () => {
             </tr>
           </thead>
           <tbody>
-            {stocks &&
-              Object.values(stocks).map(stock => (
-                <tr key={stock.symbol}>
-                  <td>{stock.symbol}</td>
-                  <td>{stock.open}</td>
-                  <td>{stock.last}</td>
-                  <td>{stock.high}</td>
-                  <td>{stock.low}</td>
-                  <td className={stock.change > 0 ? "green" : "red"}>
-                    {stock.change}
-                  </td>
-                  <td>
-                    <button onClick={() => unsubscribe(stock.symbol)}>X</button>
-                  </td>
-                </tr>
-              ))}
+            {Object.values(stocks).map(stock => (
+              <tr key={stock.symbol}>
+                <td>{stock.symbol}</td>
+                <td>{stock.open}</td>
+                <td>{stock.last}</td>
+                <td>{stock.high}</td>
+                <td>{stock.low}</td>
+                <td className={stock.change > 0 ? "green" : "red"}>
+                  {stock.change}
+                </td>
+                <td>
+                  <button onClick={() => unsubscribe(stock.symbol)}>
+                    Unsubscribe
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
